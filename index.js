62ad8caf9d234258fc2a4ad44166a58d5707bbba
shap0011 <shap0011@algonquinlive.com>
2024-12-27 11:17:15 -0500

    Adds code

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const generateRandomNumber = require('./utils');
+// import { getPosts } from "./postController.js";
+import getPosts, { getPostLength } from "./postController.js";
 
-console.log(`Random Number: ${generateRandomNumber()}`)
+console.log(getPosts());
+
+console.log(`Posts Length: ${getPostLength()}`);
+
+// const { generateRandomNumber, celsiusToFahrenheit } = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(0)}`);
