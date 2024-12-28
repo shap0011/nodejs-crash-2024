86a842241fa66b363182ab70e3eb3fe38d989d9e
shap0011 <shap0011@algonquinlive.com>
2024-12-27 23:50:10 -0500

    GET request code

--- server.js
+++ server.js
@@ -10,23 +10,28 @@ const __dirname = path.dirname(__filename);
 
 console.log(__filename, __dirname);
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     try {
         // Check if GET request
         if (req.method === 'GET') {
+            let filePath;
             if (req.url === '/') {
-                res.writeHead(200, { 'Content-Type': 'text/html'});
-                res.end('<h1>Homepage</h1>');
+                filePath = path.join(__dirname, 'public', 'index.html');
             } else if(req.url === '/about') {
-                res.writeHead(200, { 'Content-Type': 'text/html'});
-                res.end('<h1>About</h1>');       
+                filePath = path.join(__dirname, 'public', 'about.html');       
             } else {
-                res.writeHead(404, { 'Content-Type': 'text/html'});
-                res.end('<h1>Not Found</h1>');
+                // res.writeHead(404, { 'Content-Type': 'text/html'});
+                // res.end('<h1>Not Found</h1>');
+                throw new Error('Not Found');
             }
-        } else {
-            throw new Error('Method not allowed');
-        }
+
+        const data = await fs.readFile(filePath);
+        res.setHeader('Content-Type', 'text/html');
+        res.write(data);
+        res.end();
+      } else {
+        throw new Error('Method not allowed');
+      }
     } catch (error) {
         res.writeHead(500, { 'Content-Type': 'text/plain'});
         res.end('Server Error');
